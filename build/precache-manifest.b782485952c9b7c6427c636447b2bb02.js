3dffb53d7bc4f8032c4e8f298f2fc9a84acf09f3
Mukmin Abd Samad <mukminl4@gmail.com>
2020-06-10 14:00:42 +0800

    deploy to netlify: fix 404 redirect

---
+++ build/precache-manifest.b782485952c9b7c6427c636447b2bb02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94c89f27c48c1412ee440101e94bcc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1327e4829b92c074851e",
+    "url": "/static/css/main.11952c88.chunk.css"
+  },
+  {
+    "revision": "12d13fed8206c4c9cc08",
+    "url": "/static/js/2.9a930f1f.chunk.js"
+  },
+  {
+    "revision": "4cba152bee924c40068cb5f841f6f027",
+    "url": "/static/js/2.9a930f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1327e4829b92c074851e",
+    "url": "/static/js/main.ebf96898.chunk.js"
+  },
+  {
+    "revision": "e655e3fb8b84a0cfa6cb",
+    "url": "/static/js/runtime-main.53b5f9c6.js"
+  }
+]);
